954068998c1466767ca2679da1e7b7a7d2084386
Stephen Semine <stephensemine@mac-ss.local>
2026-03-13 09:50:31 +1000

    added pca func

--- scripts/sh/pipeline/covar.sh
+++ scripts/sh/pipeline/covar.sh
@@ -1,1 +1,11 @@
-#!
+#!/bin/bash
+
+source /home/s4693165/honours/config/paths.conf
+
+for n in {1..4}; do
+    echo "Running get_covar.sh for TRM $n ..."
+    /home/s4693165/honours/scripts/sh/utils/get_pca.sh \
+        --n-pca "$n" \
+        --befile "$GENE_EXP_FILTERED_DATA" \
+        --out-pca out_befile="$(dirname "$PCA_DATA")/$(basename "$PCA_DATA")_${n}"
+done
