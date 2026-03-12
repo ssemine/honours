d25c4ad2bb01fdd04979f4b27861b1f59f83a227
Stephen Semine <stephensemine@mac-ss.local>
2026-03-13 09:22:53 +1000

    added trm.sh

---
+++ scripts/sh/pipeline/trm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /home/s4693165/honours/config/paths.conf
+source "$PHENO_CONF"
+
+# make_trm.sh -> get_covar.sh -> get_pca.sh -> run_oreml.sh
+
+# tblup/trm/cuttrm
+echo "Running make_trm.sh for all data ..."
+/home/s4693165/honours/scripts/sh/utils/make_trm.sh \
+        --befile "$GENE_EXP_FILTERED_DATA" \
+        --trm-cutoff 1.00 \
+        --out-trm "$TRM_DATA"
+
+for chr in {1..29}; do
+    echo "Running make_trm.sh for chromosome $chr ..."
+    /home/s4693165/honours/scripts/sh/utils/make_trm.sh \
+        --befile "$GENE_EXP_FILTERED_DATA" \
+        --chr "$chr" \
+        --trm-cutoff 1.00 \
+        --out-trm "$TRM_DATA"
+done
