d9a5d8bcb53208af8cb7b2239ff5d664e6d9bcb8
Stephen Semine <stephensemine@mac-ss.local>
2026-03-13 14:09:25 +1000

    Added comments

--- scripts/R/tblup/iqr_outliers.R
+++ scripts/R/tblup/iqr_outliers.R
@@ -1,10 +1,12 @@
 setwd("/home/s4693165/honours/scripts/R")
 source("/home/s4693165/honours/scripts/R/tblup/orm_helper_functions.R")
 
+# Accepts command line argument for ORM file prefix
 args <- commandArgs(trailingOnly = TRUE)
 orm_path <- args[1]
 orm_obj <- ReadORMBin(orm_path)
 
+# Uses IQR to identify outliers and write to stdout
 diag_vals <- orm_obj$diag
 q <- quantile(diag_vals, c(0.25, 0.75))
 iqr <- q[2] - q[1]
