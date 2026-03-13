bdd483cb7b8c23ec9e338c361df50c0bed40ce36
ssemine <s.semine@uqconnect.edu.au>
2026-03-13 15:39:08 +1000

    temp commit

--- scripts/sh/pipeline/trm.sh
+++ scripts/sh/pipeline/trm.sh
@@ -7,20 +7,26 @@ source "$PHENO_CONF"
 
 
 # First, use GENE_EXP_FILTERED_DATA, then GENE_EXP_FINAL_DATA after outlier filtering
+
 befile="$GENE_EXP_FILTERED_DATA"
+is_intermediate=false
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --befile)
             befile="$2"
             shift 2
             ;;
+    	--intermediate)
+	    is_intermediate=true
+	    shift 1
+	    ;;
         *)
             echo "Unknown argument: $1"
             exit 1
             ;;
     esac
 done
-
+if $intermediate
 echo "Running make_trm.sh for all data ..."
 "$SH_UTILS_DIR/make_trm.sh" \
         --befile "$befile" \
