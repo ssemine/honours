53cc0c69459825c42d5ea8a3bfd729a816cc78c1
Stephen Semine <stephensemine@mac-ss.local>
2026-03-13 14:23:18 +1000

    regex fix

--- scripts/sh/pipeline/filter_trm_outliers.sh
+++ scripts/sh/pipeline/filter_trm_outliers.sh
@@ -9,7 +9,7 @@ module load "$R_MODULE"
 
 excl_iids="$EXCL_IIDS_TRM"
 out_bod="$GENE_EXP_FINAL_BOD_DATA"
-befiles=($GENE_EXP_DIR/*$GENE_EXP_FILTERED_FILENAME)
+befiles=( "$GENE_EXP_DIR"/*"$GENE_EXP_FILTERED_FILENAME"* !(*.log) )
 
 for befile in "${befiles[@]}"; do
     Rscript "$R_TBLUP_DIR/iqr_outliers.R" "$bod_file" > "$excl_iids"
