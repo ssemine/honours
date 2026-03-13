e6e9630f67a9273bb00d616484f3400fae4eea05
Stephen Semine <stephensemine@mac-ss.local>
2026-03-13 16:48:52 +1000

    removed osca.conf unnecessary var, wrote test version of filter_trm_outliers.sh

--- scripts/sh/pipeline/filter_trm_outliers.sh
+++ scripts/sh/pipeline/filter_trm_outliers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-shopt -s extglob nullglob # needed for regex
+shopt -s nullglob globstar # needed for regex
 
 source /home/s4693165/honours/config/paths.conf
 source "$OSCA_CONF"
@@ -10,14 +10,28 @@ module load "$OSCA_MODULE"
 module load "$R_MODULE"
 
 excl_iids="$EXCL_IIDS_TRM"
-out_bod="$GENE_EXP_FINAL_BOD_DATA"
-befiles=( "$GENE_EXP_DIR"/*"$GENE_EXP_FILTERED_FILENAME"* !(*.log) )
-trms=( "$TRM_DIR"/*"$GENE_EXP_FILTERED_FILENAME"* !(*.log) )
 
-for befile in "${befiles[@]}"; do
+for dir in "$INTERMEDIATE_DIR"/**/; do
+    [[ -d "$dir" ]] || continue
+
+    trm_file=("$dir"*trm*)
+    [[ -f "${trm_file[0]}" ]] || continue # skip
+
+    trm_base=$(basename "${trm_file[0]}")
+    trm_prefix="${trm_base%%trm*}trm" 
+
+    # hardcoded to avoid checking .log
+    befile=("$dir"*filtered_finalprofile.v2.oii)
+    [[ -f "${befile[0]}" ]] || continue
+
+    befile_base=$(basename "${befile[0]}")
+    befile_prefix="${befile_base%%.*}"  
+    
+    Rscript "$R_TBLUP_DIR/iqr_outliers.R" "$dir$befile_prefix" > "$excl_iids"
+    
     osca \
         --befile "$befile" \
         --remove "$excl_iids" \
         --make-bod \
-        --out "$out_bod":
-done
+        --out "$GENE_EXP_DIR/final_$befile_prefix" # figure out how to name this
+done  
