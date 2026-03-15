940201054c174788a3bb9b4934ea9dde7bc08c94
Stephen Semine <stephensemine@mac-ss.local>
2026-03-16 04:50:48 +1000

    test run.sh

--- scripts/sh/pipeline/filter.sh
+++ scripts/sh/pipeline/filter.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# exclude_sex_chr.sh -> filter_bod_pheno.sh -> filter_bod_na_pheno.sh -> map_iid_pheno.sh PHENOTYPE AND BOD PREP
-# make_trm.sh -> get_covar.sh -> get_pca.sh -> run_oreml.sh
-
 source /home/s4693165/honours/config/paths.conf
 source "$PHENO_CONF"
 
---
+++ scripts/sh/pipeline/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /home/s4693165/honours/config/paths.conf
+
+# exclude_sex_chr.sh -> filter_bod_pheno.sh -> filter_bod_na_pheno.sh -> map_iid_pheno.sh PHENOTYPE AND BOD PRE PREP
+# make_trm.sh -> (filter_trm_outliers.sh) -> get_covar.sh -> get_pca.sh -> run_oreml.sh
+
+mkdir -p "$GENE_EXP_PREPROCSESSED_DIR"
+mkdir -p "$GENE_EXP_FINAL_DIR"
+
+./filter.sh
+./trm.sh --intermediate --befile "$GENE_EXP_FILTERED_DATA" --trm-cutoff 1.00
+./filter_trm_outliers.sh
+./trm.sh --befile "$GENE_EXP_FINAL_DATA" --trm-cutoff 1.00
