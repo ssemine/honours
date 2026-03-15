885722e3ab4e76b16f1fd95ffe3e9754d7109b18
ssemine <s.semine@uqconnect.edu.au>
2026-03-16 09:07:55 +1000

    fix

--- scripts/sh/pipeline/mean_var.sh
+++ scripts/sh/pipeline/mean_var.sh
@@ -6,5 +6,5 @@ source /home/s4693165/honours/config/paths.conf
 "$SH_UTILS_DIR/get_mean_var.sh" --befile $GENE_EXP_AUTO_DATA --out $PROBES_DIR/initial_auto
 
 # Call probes from final filtered with 1.00 relatedness
-final_data="$GENE_EXP_FINAL_DIR/final_cut1.00_filtered_finalprofile"
+final_data="$GENE_EXP_FINAL_DIR/final_cut1.00_filtered_finalprofile.v2"
 "$SH_UTILS_DIR/get_mean_var.sh" --befile $final_data --out $PROBES_DIR/final
