12dd6aa46b06fe2a11c4693dd045b5d02082868c
ssemine <s.semine@uqconnect.edu.au>
2026-03-16 09:28:26 +1000

    fix

--- scripts/R/tblup/probes_mean_var.R
+++ scripts/R/tblup/probes_mean_var.R
@@ -5,25 +5,32 @@ setwd(working_dir)
 
 mean_path_initial <- file.path(probe_dir, "initial_auto.mean.txt")
 var_path_initial  <- file.path(probe_dir, "initial_auto.var.txt")
+mean_path_std <- file.path(probe_dir, "std_auto.mean.txt")
+var_path_std <- file.path(probe_dir, "std_auto.var.txt")
 mean_path_final <- file.path(probe_dir, "final.mean.txt")
 var_path_final <- file.path(probe_dir, "final.var.txt")
 
 mean_df_initial <- read.table(mean_path_initial, header = FALSE)
 var_df_initial  <- read.table(var_path_initial,  header = FALSE)
+mean_df_std <- read.table(mean_path_std, header = FALSE)
+var_df_std <- read.table(var_path_std, header = FALSE)
 mean_df_final <- read.table(mean_path_final, header=FALSE)
 var_df_final <- read.table(var_path_final, header=FALSE)
 
 colnames(mean_df_initial) <- c("probe", "mean")
 colnames(var_df_initial)  <- c("probe", "variance")
+colnames(mean_df_std) <- c("probe", "mean")
+colnames(var_df_std) <- c("probe", "mean")
 colnames(mean_df_final) <- c("probe", "mean")
 colnames(var_df_final) <- c("probe", "variance")
 
 df_initial <- merge(mean_df_initial, var_df_initial, by = "probe")
+df_std <- merge(mean_df_std, var_df_std, by "probe")
 df_final <- merge(mean_df_final, var_df_final, by = "probe")
 
 ggplot(df_initial, aes(x = mean, y = variance)) +
   geom_point(alpha = 0.3, color = "blue") +
-  geom_point(data = df_final, aes(x = mean, y = variance), color="red", alpha=0.5) +
+  geom_point(data = df_std, aes(x = mean, y = variance), color="red", alpha=0.5) +
   labs(
     title = "Mean vs Variance of Probes",
     x = expression(mu),
