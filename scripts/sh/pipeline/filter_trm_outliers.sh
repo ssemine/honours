0ab0c49ccdec0d87a7cfbb3121446f7572c81761
Stephen Semine <stephensemine@mac-ss.local>
2026-03-16 04:08:20 +1000

    test

--- scripts/sh/pipeline/filter_trm_outliers.sh
+++ scripts/sh/pipeline/filter_trm_outliers.sh
@@ -27,11 +27,12 @@ for dir in "$INTERMEDIATE_DIR"/**/; do
     befile_base=$(basename "${befile[0]}")
     befile_prefix="${befile_base%%.*}"  
     
+    echo "$dir$trm_prefix"
     Rscript "$R_TBLUP_DIR/iqr_outliers.R" "$dir$trm_prefix" > "$excl_iids"
     
-    osca \
-        --befile "$dir$befile_prefix" \
-        --remove "$excl_iids" \
-        --make-bod \
-        --out "$GENE_EXP_DIR/final_$befile_prefix" # figure out how to name this
-done  
+    #osca \
+    #    --befile "$dir$befile_prefix" \
+    #    --remove "$excl_iids" \
+    #    --make-bod \
+    #    --out "$GENE_EXP_DIR/final_$befile_prefix" # figure out how to name this
+done
