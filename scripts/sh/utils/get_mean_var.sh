bbf0ff1b54fee239c10af96b8bf8f9a11e006b53
Stephen Semine <stephensemine@mac-ss.local>
2026-03-16 09:03:00 +1000

    Fix

--- scripts/sh/utils/get_mean_var.sh
+++ scripts/sh/utils/get_mean_var.sh
@@ -15,10 +15,10 @@ while [[ $# -gt 0 ]]; do
             befile="$2"
             shift 2
             ;;
-	--out)
-		out="$"
-		shift 2
-		;;
+        --out)
+            out="$2"
+            shift 2
+            ;;
         *)
             echo "Unknown argument: $1"
             exit 1
