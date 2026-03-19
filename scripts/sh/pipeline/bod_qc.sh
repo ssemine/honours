cb6ca416bd53506d8497ed887296dc43c3bb241c
Stephen Semine <s.semine@uqconnect.edu.au>
2026-03-20 02:53:44 +1000

    fix

--- scripts/sh/pipeline/bod_qc.sh
+++ scripts/sh/pipeline/bod_qc.sh
@@ -15,7 +15,7 @@ while [[ $# -gt 0 ]]; do
             befile="$2"
             shift 2
             ;;
-    	--log2-transform)
+        --log2-transform)
             log2_transform=true
             shift 1
             ;;
@@ -33,9 +33,11 @@ done
 txt_profile="$INTERMEDIATE_DIR/$befile.txt"
 txt_profile_transformed="$INTERMEDIATE_DIR/$befile.transformed.txt"
 
-if $log2_transform; then
+if [ "$log2_transform" = true ]; then
     osca --befile "$befile" --make-efile --out "$txt_profile"
-    Rscript "/home/s4693165/honours/scripts/R/tblup/utils/cpm_transform.R" "$txt_profile" > "$txt_profile_transformed"
+    Rscript "/home/s4693165/honours/scripts/R/tblup/utils/cpm_transform.R" \
+        --input "$txt_profile" \
+        --output "$txt_profile_transformed"
     osca --efile "$txt_profile_transformed" --gene-expression --make-bod --out "$out_bod"
     osca --befile "$out_bod" --update-opi "$befile.opi"
 fi
