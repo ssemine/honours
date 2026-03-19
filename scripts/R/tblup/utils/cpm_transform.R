b88d6edec21fd56959926f22271d41bfced86b39
Stephen Semine <s.semine@uqconnect.edu.au>
2026-03-20 05:00:40 +1000

    r fix

--- scripts/R/tblup/utils/cpm_transform.R
+++ scripts/R/tblup/utils/cpm_transform.R
@@ -7,57 +7,28 @@ if (!requireNamespace("data.table", quietly = TRUE)) {
 library(data.table)
 
 args <- commandArgs(trailingOnly = TRUE)
-input_file <- NULL
-output_file <- NULL
-
-i <- 1
-while (i <= length(args)) {
-  if (args[i] == "--input") {
-    input_file <- args[i + 1]
-    i <- i + 2
-  } else if (args[i] == "--output") {
-    output_file <- args[i + 1]
-    i <- i + 2
-  } else {
-    stop(paste("Unknown argument:", args[i]))
-  }
-}
-
-if (is.null(input_file) || is.null(output_file)) {
-  stop("Usage: Rscript log2_cpm.R --input input_file.txt --output output_file.txt")
-}
+input_file <- args[which(args == "--input") + 1]
+output_file <- args[which(args == "--output") + 1]
 
 cat("Reading input file:", input_file, "\n")
-
-dt <- fread(
-  input_file,
-  sep = "",       # whitespace-separated
-  header = TRUE,
-  data.table = TRUE,
-  stringsAsFactors = FALSE,  # safety
-  showProgress = TRUE
-)
+dt <- fread(input_file, header = TRUE, fill = TRUE, data.table = TRUE)
 
 gene_cols <- setdiff(names(dt), c("IID", "FID"))
 
-cat("Applying log2(CPM + 1) transformation...\n")
+cat("Applying log2(CPM + 1) transformation in chunks...\n")
 
-dt[, (gene_cols) := lapply(.SD, function(x) {
-  x <- suppressWarnings(as.numeric(x))
-  x[is.na(x) | is.nan(x) | is.infinite(x)] <- 0
-  log2(x + 1)
-}), .SDcols = gene_cols]
+chunk_size <- 1000   # number of columns per chunk
+for (start in seq(1, length(gene_cols), by = chunk_size)) {
+  end <- min(start + chunk_size - 1, length(gene_cols))
+  cols_chunk <- gene_cols[start:end]
+
+  dt[, (cols_chunk) := lapply(.SD, function(x) {
+    x <- suppressWarnings(as.numeric(x))
+    x[is.na(x) | is.nan(x) | is.infinite(x)] <- 0
+    log2(x + 1)
+  }), .SDcols = cols_chunk]
+}
 
 cat("Writing output to:", output_file, "\n")
-
-write.table(
-  dt,
-  file = output_file,
-  sep = "\t",
-  quote = FALSE,
-  row.names = FALSE,
-  col.names = TRUE,
-  na = "0"
-)
-
+fwrite(dt, file = output_file, sep = "\t", quote = FALSE, na = "0")
 cat("Done!\n")
