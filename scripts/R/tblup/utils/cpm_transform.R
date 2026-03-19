6451035b76cbb6d44e090eb99cce6a591c3f6a48
Stephen Semine <s.semine@uqconnect.edu.au>
2026-03-20 02:39:59 +1000

    test

---
+++ scripts/R/tblup/utils/cpm_transform.R
@@ -0,0 +1,35 @@
+library(data.table)
+
+args <- commandArgs(trailingOnly = TRUE)
+input_file <- NULL
+output_file <- NULL
+
+i <- 1
+while (i <= length(args)) {
+  if (args[i] == "--input") {
+    input_file <- args[i + 1]
+    i <- i + 2
+  } else if (args[i] == "--output") {
+    output_file <- args[i + 1]
+    i <- i + 2
+  } else {
+    stop(paste("Unknown argument:", args[i]))
+  }
+}
+
+if (is.null(input_file) || is.null(output_file)) {
+  stop("Usage: Rscript log2_cpm.R --input input_file.txt --output output_file.txt")
+}
+
+cat("Reading input file:", input_file, "\n")
+dt <- fread(input_file)
+
+gene_cols <- setdiff(names(dt), c("IID", "FID"))
+
+cat("Applying log2(CPM + 1) transformation...\n")
+dt[, (gene_cols) := lapply(.SD, function(x) log2(x + 1)), .SDcols = gene_cols]
+
+cat("Writing output to:", output_file, "\n")
+fwrite(dt, output_file, sep = "\t")
+
+cat("Done!\n")
---
+++ scripts/sh/pipeline/bod_qc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source /home/s4693165/honours/config/paths.conf
+source "$OSCA_CONF"
+source "$R_CONF"
+
+module load "$OSCA_MODULE"
+module load "$R_MODULE"
+
+log2_transform=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --befile)
+            befile="$2"
+            shift 2
+            ;;
+    	--log2_transform)
+            log2_transform=true
+            shift 1
+            ;;
+        --out-bod)
+            out_bod="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown argument: $1"
+            exit 1
+            ;;
+    esac
+done
+
+txt_profile="$INTERMEDIATE_DIR/$befile.txt"
+txt_profile_transformed="$INTERMEDIATE_DIR/$befile.transformed.txt"
+
+if log2_transform; then
+    osca --befile "$befile" --make-efile --out "$txt_profile"
+    Rscript "/home/s4693165/honours/scripts/R/tblup/utils/cpm_transform.R" "$txt_profile" > "$txt_profile_transformed"
+    osca --efile "$txt_profile_transformed" --gene-expression --make-bod --out "$out_bod"
+    osca --befile "$out_bod" --update-opi "$befile.opi"
+fi
