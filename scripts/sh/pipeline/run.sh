5b9be8e5bf6790f1c434804221398963a355a5a8
Stephen Semine <s.semine@uqconnect.edu.au>
2026-03-20 09:02:03 +1000

    fix

--- scripts/sh/pipeline/run.sh
+++ scripts/sh/pipeline/run.sh
@@ -49,7 +49,8 @@ missing_ratio_probe=0.05
 
 
 # add filtering by PCA1
-Rscript "$R_SCRIPTS_DIR/tblup/utils/filter_pca1.R" --input "$initial_befile" --thresh -150 > "$INTERMEDIATE_DIR/pca1_excl_iids.list"
+osca --befile "$initial_befile" --make-efile --out "$initial_befile.txt"
+Rscript "$R_SCRIPTS_DIR/tblup/utils/filter_pca1.R" --input "$initial_befile.txt" --thresh -150 > "$INTERMEDIATE_DIR/pca1_excl_iids.list"
 osca --befile "$initial_befile" --remove "$INTERMEDIATE_DIR/pca1_excl_iids.list" --make-bod "$initial_befile.tmp"
 initial_befile="$initial_befile.tmp"
 
