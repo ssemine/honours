8e0f8b01fe4dd3c24632dd448355bbd141e362ab
ssemine <s.semine@uqconnect.edu.au>
2026-03-20 10:00:37 +1000

    r

--- scripts/R/tblup/plot_rsq.R
+++ scripts/R/tblup/plot_rsq.R
@@ -2,10 +2,15 @@ library(tidyverse)
 
 # --- Hardcoded input files ---
 input_files <- c(
+  "/scratch/user/s4693165/results/tblup_final_1.00_pca_4.rsq",
+  "/scratch/user/s4693165/results/tblup_final_0.75_pca_4.rsq",
+  "/scratch/user/s4693165/results/tblup_final_0.5_pca_4.rsq",
+  "/scratch/user/s4693165/results/tblup_final_0.25_pca_4.rsq",
+  "/scratch/user/s4693165/results/tblup_final_0.1_pca_4.rsq"
 )
 
 # --- Output file ---
-output_file <- "/scratch/user/s4693165/results/herit_plot.pdf"
+output_file <- "~/honours/data/plots/tblup/reml_pca3.png"
 
 # --- Read each .rsq and extract V(O)/Vp ---
 herit_df <- map_dfr(input_files, function(f) {
@@ -25,12 +30,13 @@ p <- ggplot(herit_df, aes(x = Index, y = h2)) +
   geom_point(size = 3) +
   geom_line(group = 1) +
   geom_errorbar(aes(ymin = h2 - SE, ymax = h2 + SE), width = 0.2, alpha = 0.5) +
-  theme_minimal() +
+  theme_bw() +
   scale_x_continuous(breaks = herit_df$Index, labels = herit_df$File) +
-  labs(x = "Input file", y = expression(h^2), title = "Heritability (V(O)/Vp) across files") +
+  labs(x = "Input file", y = expression(h^2), title = "V(T)/V(P)") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 # --- Save plot ---
 ggsave(output_file, p, width = 8, height = 5)
 
 cat("Plot saved to:", output_file, "\n")
+
