dca82acfe4cfa66b88a6808b3c01fa85a378ef09
ssemine <s.semine@uqconnect.edu.au>
2026-03-23 08:41:31 +1000

    fix

--- scripts/slurm/slurm_run.sh
+++ scripts/slurm/slurm_run.sh
@@ -4,6 +4,9 @@ source /home/s4693165/honours/config/paths.conf
 source "$SLURM_CONF"
 source "$PHENO_CONF"
 
+arg1="--use-covar-idx-file"
+arg2="--covar-idx-file"
+arg3="/home/s4693165/honours/scripts/slurm/covars.list"
 for n in $(seq 0.00 0.05 1.00); do
 	sbatch \
 		--account="$ACCOUNT_STRING" \
@@ -14,5 +17,5 @@ for n in $(seq 0.00 0.05 1.00); do
 		--output="$LOGS_TBLUP_DIR/%x_%j.out" \
 		--error="$LOGS_TBLUP_DIR/%x_%j.err" \
 		--job-name="oreml_${n}" \
-		--wrap="/home/s4693165/honours/scripts/sh/pipeline/run.sh --covar-idx-file /home/s4693165/honours/scripts/slurm/covars.list --n-pca 3 --trm-cutoff ${n} --use-covar"
+		--wrap="/home/s4693165/honours/scripts/sh/pipeline/run.sh ${arg1} ${arg2} ${arg3} --trm-cutoff ${n}"
 done
