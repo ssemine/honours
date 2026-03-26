10f364abe8f751cb1da02ac2fd2ecd1f3efcbe89
ssemine <s.semine@uqconnect.edu.au>
2026-03-26 10:12:05 +1000

    plot diff

--- scripts/R/tblup/utils/pca_plot.R
+++ scripts/R/tblup/utils/pca_plot.R
@@ -1,10 +1,16 @@
-# Load necessary library
 library(ggplot2)
+setwd("/home/s4693165/honours/scripts/R")
+source("tblup/utils/read_orm.R")
 
-# Path to your final matrix (IID, FID, Genes)
-# Path to your final matrix (IID, FID, Genes)
-input_file <- "/scratch/user/s4693165/results/cut_1.99/efile"
-
+input_file <- "/scratch/user/s4693165/results/cut_nocut_3_pca2/efile"
+orm_obj <- ReadORMBin("/scratch/user/s4693165/results/cut_nocut_3_pca2/trm")
+n <- nrow(orm_obj$id)
+ORM_mat <- matrix(0, n, n)
+diag(ORM_mat) <- orm_obj$diag
+ORM_mat[lower.tri(ORM_mat)] <- orm_obj$off
+ORM_mat[upper.tri(ORM_mat)] <- t(ORM_mat)[upper.tri(ORM_mat)]
+rownames(ORM_mat) <- orm_obj$id[,1]
+colnames(ORM_mat) <- orm_obj$id[,1]
 # Read the data
 expr <- read.table(input_file, header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
 
@@ -29,13 +35,45 @@ pca_res <- prcomp(gene_expr_num, center = TRUE, scale. = TRUE)
 scores <- data.frame(iid_data, PC1 = pca_res$x[,1], PC2 = pca_res$x[,2])
 head(scores)
 
-# Simple scatter plot
-p <- ggplot(scores, aes(x = PC1, y = PC2)) +
-  geom_point(color = "blue", size = 2) +
+# Get mean ORM per individual
+orm_mean <- rowMeans(ORM_mat)
+
+# Put into a data frame
+orm_mean <- rowMeans(ORM_mat)
+
+orm_df <- data.frame(
+  iid = rownames(ORM_mat),
+  orm_mean = orm_mean
+)
+colnames(scores)[colnames(scores) == "IID"] <- "iid"
+# Merge with scores
+scores2 <- merge(scores, orm_df, by = "iid")
+
+library(ggrepel)  # better for overlapping labels
+library(dplyr)
+# Filter points to label
+labels_df <- scores2 %>%
+  filter(orm_mean > 0.35 | PC1 < -100)
+
+# Original plot with conditional labels
+p <- ggplot(scores2, aes(x = PC1, y = PC2, color = orm_mean)) +
+  geom_point(size = 2, alpha = 0.5) +
+  scale_color_viridis_c() +
   xlab(paste0("PC1 (", round(summary(pca_res)$importance[2,1]*100, 1), "%)")) +
   ylab(paste0("PC2 (", round(summary(pca_res)$importance[2,2]*100, 1), "%)")) +
-  ggtitle("PCA of Final Gene Expression Matrix") +
-  theme_minimal()
+  ggtitle("PCA of Final Gene Expression Matrix (colored by mean ORM)") +
+  geom_text_repel(
+    data = labels_df,
+    aes(label = iid),
+    size = 3,
+    color = "white",        # always visible
+    bg.color = "black",   # background behind text
+    bg.r = 0.15,          # roundness of background box
+    box.padding = 0.3,
+    point.padding = 0.3,
+    max.overlaps = Inf
+  ) +
+  theme_bw()
 
-o1 <- "~/honours/data/plots/tblup/pca_dis.png"
-ggsave(o1, p, width = 8, height = 5)
+print(p)
+
