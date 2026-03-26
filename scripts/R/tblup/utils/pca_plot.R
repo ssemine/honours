16581e5ddb5af9d61f814d25dfcd0681925c1e49
Stephen Semine <stephensemine@mac-ss.local>
2026-03-26 10:13:42 +1000

    polish

--- scripts/R/tblup/utils/pca_plot.R
+++ scripts/R/tblup/utils/pca_plot.R
@@ -1,4 +1,7 @@
 library(ggplot2)
+library(ggrepel)
+library(dplyr)
+
 setwd("/home/s4693165/honours/scripts/R")
 source("tblup/utils/read_orm.R")
 
@@ -11,51 +14,33 @@ ORM_mat[lower.tri(ORM_mat)] <- orm_obj$off
 ORM_mat[upper.tri(ORM_mat)] <- t(ORM_mat)[upper.tri(ORM_mat)]
 rownames(ORM_mat) <- orm_obj$id[,1]
 colnames(ORM_mat) <- orm_obj$id[,1]
-# Read the data
+
+input_file <- "/scratch/user/s4693165/results/cut_nocut_3_pca2/efile"
 expr <- read.table(input_file, header = TRUE, sep = " ", check.names = FALSE, stringsAsFactors = FALSE)
-
-# Separate IID/FID from expression data
-iid_data <- expr[, 1:2]   # Columns 1 and 2 are IID/FID
-gene_expr <- expr[, -c(1,2)]  # Remaining columns
-
-# Ensure all gene expression columns are numeric
+iid_data <- expr[, 1:2]
+gene_expr <- expr[, -c(1,2)]
 gene_expr_num <- data.frame(lapply(gene_expr, function(x) as.numeric(trimws(x))))
-
-# Check for conversion problems
 if (any(is.na(gene_expr_num))) {
   cat("Warning: NAs introduced during conversion. Number of NAs:", sum(is.na(gene_expr_num)), "\n")
-  # Optionally, remove rows with NA
   gene_expr_num <- na.omit(gene_expr_num)
 }
 
-# Perform PCA
 pca_res <- prcomp(gene_expr_num, center = TRUE, scale. = TRUE)
-
-# Extract first two PCs
 scores <- data.frame(iid_data, PC1 = pca_res$x[,1], PC2 = pca_res$x[,2])
 head(scores)
 
-# Get mean ORM per individual
 orm_mean <- rowMeans(ORM_mat)
-
-# Put into a data frame
 orm_mean <- rowMeans(ORM_mat)
-
 orm_df <- data.frame(
   iid = rownames(ORM_mat),
   orm_mean = orm_mean
 )
 colnames(scores)[colnames(scores) == "IID"] <- "iid"
-# Merge with scores
-scores2 <- merge(scores, orm_df, by = "iid")
 
-library(ggrepel)  # better for overlapping labels
-library(dplyr)
-# Filter points to label
+scores2 <- merge(scores, orm_df, by = "iid")
 labels_df <- scores2 %>%
   filter(orm_mean > 0.35 | PC1 < -100)
 
-# Original plot with conditional labels
 p <- ggplot(scores2, aes(x = PC1, y = PC2, color = orm_mean)) +
   geom_point(size = 2, alpha = 0.5) +
   scale_color_viridis_c() +
@@ -66,9 +51,9 @@ p <- ggplot(scores2, aes(x = PC1, y = PC2, color = orm_mean)) +
     data = labels_df,
     aes(label = iid),
     size = 3,
-    color = "white",        # always visible
-    bg.color = "black",   # background behind text
-    bg.r = 0.15,          # roundness of background box
+    color = "white",       
+    bg.color = "black",  
+    bg.r = 0.15,         
     box.padding = 0.3,
     point.padding = 0.3,
     max.overlaps = Inf
