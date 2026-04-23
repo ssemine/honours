1e640e7ce3a09960815e6024f2306de3ef249dcc
Stephen Semine <stephensemine@mac-ss.local>
2026-04-23 13:38:32 +1000

    fix

--- scripts/slurm/slurm_run_gblup.sh
+++ scripts/slurm/slurm_run_gblup.sh
@@ -11,15 +11,16 @@ arg3="/home/s4693165/honours/scripts/slurm/$c_file"
 
 arg4="--covars"
 arg5="3 5"
-for n in $(seq 0.00 0.05 1.00); do
+n="1.00"
+#for n in $(seq 0.00 0.05 1.00); do
 	sbatch \
 		--account="$ACCOUNT_STRING" \
 		--partition="$PARTITION" \
 		--cpus-per-task="$CPUS" \
 		--mem="$MEM_RUN_TBLUP" \
 		--time="$TIME" \
-		--output="$LOGS_TBLUP_DIR/%x_%j.out" \
-		--error="$LOGS_TBLUP_DIR/%x_%j.err" \
+		--output="$LOGS_GBLUP_DIR/%x_%j.out" \
+		--error="$LOGS_GBLUP_DIR/%x_%j.err" \
 		--job-name="gblup_${n}" \
 		--wrap="/home/s4693165/honours/scripts/sh/pipeline/gblup/run_gblup.sh --n-pca 2 ${arg1} ${arg2} ${arg3} --grm-cutoff ${n}"
-done
+#done
